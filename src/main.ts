2f43a6d785f3b4e725098e23c02668fa793f1959
hezhongfeng <287645587@qq.com>
2024-08-08 14:21:20 +0800

    ✨ feat(add test):

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './pipe/validation.pipe';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  await app.listen(3001);
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors();
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(3000);
 }
 bootstrap();
