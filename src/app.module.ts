537fe10639fe0a8ddb9b32d79531fabb7a06cb19
hezhongfeng <287645587@qq.com>
2024-08-08 14:03:51 +0800

    ✨ feat: use userr

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { Work } from './work/work.entity';
+import { Userr } from './user.entity';
+// import { User } from './user/user.entity';
+// import { Work } from './work/work.entity';
 // import { UserModule } from './user/user.module';
 // import { WorkModule } from './work/work.module';
 import { config } from 'dotenv';
@@ -23,7 +24,7 @@ const envConfig = config();
       username: envConfig.parsed.DB_USER || process.env.DB_USER,
       password: envConfig.parsed.DB_PASSWORD || process.env.DB_PASSWORD,
       database: envConfig.parsed.DB_DB || process.env.DB_DB,
-      entities: [User, Work],
+      entities: [Userr],
       synchronize: true,
     }),
   ],
