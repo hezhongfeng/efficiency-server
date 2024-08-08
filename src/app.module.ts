bcc6e9d114a018dabb055fac1c6b84d74b694a7c
hezhongfeng <287645587@qq.com>
2024-08-08 13:54:20 +0800

    ✨ feat(add test):

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { Work } from './work/work.entity';
-import { UserModule } from './user/user.module';
-import { WorkModule } from './work/work.module';
-import { config } from 'dotenv';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './user/user.entity';
+// import { Work } from './work/work.entity';
+// import { UserModule } from './user/user.module';
+// import { WorkModule } from './work/work.module';
+// import { config } from 'dotenv';
 
 // 读取 .env 文件
-const envConfig = config();
+// const envConfig = config();
 
 @Module({
   imports: [
-    UserModule,
-    WorkModule,
-    TypeOrmModule.forRoot({
-      ssl: Boolean(envConfig.parsed.DB_SSL || process.env.DB_SSL),
-      type: 'postgres',
-      host: envConfig.parsed.DB_HOST || process.env.DB_HOST,
-      port: Number(envConfig.parsed.DB_PORT || process.env.DB_PORT),
-      username: envConfig.parsed.DB_USER || process.env.DB_USER,
-      password: envConfig.parsed.DB_PASSWORD || process.env.DB_PASSWORD,
-      database: envConfig.parsed.DB_DB || process.env.DB_DB,
-      entities: [User, Work],
-      synchronize: true,
-    }),
+    // UserModule,
+    // WorkModule,
+    // TypeOrmModule.forRoot({
+    //   ssl: Boolean(envConfig.parsed.DB_SSL || process.env.DB_SSL),
+    //   type: 'postgres',
+    //   host: envConfig.parsed.DB_HOST || process.env.DB_HOST,
+    //   port: Number(envConfig.parsed.DB_PORT || process.env.DB_PORT),
+    //   username: envConfig.parsed.DB_USER || process.env.DB_USER,
+    //   password: envConfig.parsed.DB_PASSWORD || process.env.DB_PASSWORD,
+    //   database: envConfig.parsed.DB_DB || process.env.DB_DB,
+    //   entities: [User, Work],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './pipe/validation.pipe';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
+// import { ValidationPipe } from './pipe/validation.pipe';
+// import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors();
+  // app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
